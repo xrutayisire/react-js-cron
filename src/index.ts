cc13e57ecbae6ce5da8edce335386eff9eb23689
Xavier Rutayisire <xavier.rutayisire@gmail.com>
2020-05-27 16:20:30 +0200

    Support import with alias

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,9 @@ import Cron from './Cron'
 
 export * from './types'
 
+// Support "import { Cron } from 'react-js-cron'"
+// Support "import { Cron as ReactJSCron } from 'react-js-cron'"
+export { Cron }
+
+// Support "import Cron from 'react-js-cron'"
 export default Cron
