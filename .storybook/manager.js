7a46befc32241fa9d7fdde8414f8403d80c258d3
xrutayisire <xavier.rutayisire@gmail.com>
2022-11-24 08:15:11 +0000

    Deploying to gh-pages from @ xrutayisire/react-js-cron@ea49f69548a15de2ae55896d55a300c0685c5623 🚀

--- .storybook/manager.js
+++
@@ -1,8 +0,0 @@
-import addons from '@storybook/addons'
-import { STORY_RENDERED } from '@storybook/core-events'
-
-addons.register('TitleAddon', (api) => {
-  api.on(STORY_RENDERED, () => {
-    document.title = 'ReactJS Cron'
-  })
-})
