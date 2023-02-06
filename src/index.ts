77e6f9307070bfd4325977091d67edad1d48eff8
Nathan Trujillo <35079898+useafterfree@users.noreply.github.com>
2023-02-06 14:51:37 -0800

    export convertor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import Cron from './Cron'
 
+import * as convertor from './converter'
+
 export * from './types'
 
 // Support "import { Cron } from 'react-js-cron'"
 // Support "import { Cron as ReactJSCron } from 'react-js-cron'"
-export { Cron }
+export { Cron, convertor }
 
 // Support "import Cron from 'react-js-cron'"
 export default Cron
